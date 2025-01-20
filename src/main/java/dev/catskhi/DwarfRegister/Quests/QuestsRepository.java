405713aa6859498fb57b3eca989bd9742bb354c8
catskhi <catskhi@proton.me>
2025-01-20 18:03:30 -0300

    feat:Adding dwarfs repository, dwarfs service, dwarfs repository

---
+++ src/main/java/dev/catskhi/DwarfRegister/Quests/QuestsRepository.java
@@ -0,0 +1,7 @@
+package dev.catskhi.DwarfRegister.Quests;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuestsRepository extends JpaRepository<QuestsModel, Long> {
+
+}
