8324cbed85315e49ee78a6e2a53b71471c3c794a
catskhi <catskhi@proton.me>
2025-01-21 20:12:31 -0300

    feat: Update controller with data mock

--- src/main/java/dev/catskhi/DwarfRegister/Dwarfs/DwarfController.java
+++ src/main/java/dev/catskhi/DwarfRegister/Dwarfs/DwarfController.java
@@ -1,8 +1,6 @@
 package dev.catskhi.DwarfRegister.Dwarfs;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -11,5 +9,35 @@ public class DwarfController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello";
+    }
+
+    // Add Dwarf (Create)
+    @PostMapping("/create")
+    public String createDwarf() {
+        return "Dwarf created";
+    }
+
+    // Show all dwarfs (Read)
+    @GetMapping("/all")
+    public String showAllDwarfs() {
+        return "All dwarfs";
+    }
+
+    // Search Dwarf by Id (Read)
+    @GetMapping("/dwarfID")
+    public String showAllDwarfsById() {
+        return "Show all dwarfs by id";
+    }
+
+    // Update dwarf data (Update)
+    @PutMapping("/updateDwarf")
+    public String updateDwarf() {
+        return "Updated Dwarf";
+    }
+
+    // Delete dwarf (Delete)
+    @DeleteMapping("/deleteDwarf")
+    public String deleteDwarf() {
+        return "Deleted Dwarf";
     }
 }
