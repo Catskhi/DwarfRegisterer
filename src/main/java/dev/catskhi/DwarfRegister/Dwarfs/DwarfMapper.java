1fb0d072f18c0ffa0766f5fc7ef877284b39e828
catskhi <catskhi@proton.me>
2025-01-25 00:45:48 -0300

    chore: Added entity version and manually mapped DwarfMapper in entity x dto paths

--- src/main/java/dev/catskhi/DwarfRegister/Dwarfs/DwarfMapper.java
+++ src/main/java/dev/catskhi/DwarfRegister/Dwarfs/DwarfMapper.java
@@ -1,4 +1,35 @@
 package dev.catskhi.DwarfRegister.Dwarfs;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class DwarfMapper {
+
+    public DwarfModel map(DwarfDTO dwarfDTO) {
+        DwarfModel dwarfModel = new DwarfModel();
+        dwarfModel.setId(dwarfDTO.getId());
+        dwarfModel.setName(dwarfDTO.getName());
+        dwarfModel.setEmail(dwarfDTO.getEmail());
+        dwarfModel.setImgUrl(dwarfDTO.getImgUrl());
+        dwarfModel.setAge(dwarfDTO.getAge());
+        dwarfModel.setQuests(dwarfDTO.getQuests());
+        dwarfModel.setRank(dwarfDTO.getRank());
+
+        return dwarfModel;
+    }
+
+    public DwarfDTO map(DwarfModel dwarfModel) {
+
+        DwarfDTO dwarfDTO = new DwarfDTO();
+        dwarfDTO.setId(dwarfModel.getId());
+        dwarfDTO.setName(dwarfModel.getName());
+        dwarfDTO.setEmail(dwarfModel.getEmail());
+        dwarfDTO.setImgUrl(dwarfModel.getImgUrl());
+        dwarfDTO.setImgUrl(dwarfModel.getImgUrl());
+        dwarfDTO.setQuests(dwarfModel.getQuests());
+        dwarfDTO.setRank(dwarfModel.getRank());
+
+        return dwarfDTO;
+    }
+
 }
